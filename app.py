566cb46e67fb4585243c445ddf28b821e8e78d12
SayantaN <sayantanhaldar69@gmail.com>
2024-01-19 17:35:59 +0530

    first commit of weather application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, render_template, request
+from json import dumps
 app = Flask(__name__)
 
 
@@ -7,21 +8,32 @@ url = "https://api.openweathermap.org/data/2.5/weather"
 apikey = "2cd5682b8204d1e16c250776bda23fad"
 
 
-@app.route("/")
+# @app.route("/")
 def show_form():
-    return render_template("index.html")
+    # return render_template("index.html")
+    pass
 
-@app.route('/weatherapp', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def weatherapp():
-    city_name = request.form.get("city")
-    param = {
-        "q":city_name,
-        "units":"metric",
-        "apikey":apikey
-    }
-    response = requests.get(url, param)
-    data = response.json()
-    return f"{data}, city: {data['name']}"
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        city_name = request.form.get("city")
+        param = {
+            "q":city_name,
+            "units":"metric",
+            "apikey":apikey
+        }
+        response = requests.get(url, param)
+        if response.status_code >= 200 and response.status_code<300:
+            data = response.json()
+            t = data['main']
+            my_data={}
+            my_data['temp'] = t['temp']
+            my_data["city_name"] = data['name']
+            return render_template('index.html', data=my_data)
+        else:
+            return render_template('index.html', error="City Name Not Found")
 
 
 
